0f94ca846a7f8463e977ca2c73a0bdfa2354572e
Tarun Singhal <tarun.singhal@osscube.com>
2015-11-16 13:41:39 +0530

    setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+      name = 'bblogger',
+      version='1.0',
+      author='Tarun',
+      author_email='ks.tarun@gmail.com',
+      packages=['bblogger'],
+      url='https://github.com/tarun-singhal/PyLogger',
+      licence='',
+      description='',
+      long_description='',
+      zip_safe=FALSE 
+      )
+
