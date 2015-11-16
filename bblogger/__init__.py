ca4f8a1594ca7eb20e1bb362c794bf1d7e04ca0c
Tarun Kumar Singhal <ks.tarun@gmail.com>
2015-11-16 14:49:55 +0530

    Update __init__.py

---
+++ bblogger/__init__.py
@@ -0,0 +1,7 @@
+from bblogger import utils, admin, templatetags, forms
+
+VERSION = (0, 4, 5)
+
+get_version = lambda: '.'.join(map(str, VERSION))
+
+__all__ = ['utils', 'admin', 'templatetags', 'forms']
